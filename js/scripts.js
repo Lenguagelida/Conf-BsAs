c85a1e125a60e5fa901922864c42fef296f7b952
DanielDab <dab777799@gmail.com>
2023-05-27 16:35:03 -0300

    Linkeo hacia la pagina de compra de tickets

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+//Linkea el boton de comprar tickets, que esta en el carrusel, hacia la página de compra.
+let goToBuyTickets = document.getElementById("btnBuyTickets")
+goToBuyTickets.addEventListener('click', ()=>{window.location.href = 'pages/ticketPage.html'})
+
+
+
+//console.log(goToBuyTickets);
