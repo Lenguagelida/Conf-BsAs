1a922827da3765b2affc3bdbf29085a07a9c4c1b
DanielDab <dab777799@gmail.com>
2023-05-28 20:35:29 -0300

    Página de tickets completa

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,39 @@
 //Linkea el boton de comprar tickets, que esta en el carrusel, hacia la página de compra.
-let goToBuyTickets = document.getElementById("btnBuyTickets")
-goToBuyTickets.addEventListener('click', ()=>{window.location.href = 'pages/ticketPage.html'})
+function irAPaginaDeCompra() {
+	window.location.href = "pages/ticketPage.html";
+}
 
+//Declaraciones para el cálculo del monto de la entrada.
+let nodoCantidadTickets = document.getElementById("inputCantidad");
+let cantTickets = 1
+nodoCantidadTickets.addEventListener('change', ()=>{
+    cantTickets = parseInt(nodoCantidadTickets.value);
+});
 
+let categoria = document.getElementById("selectCategoria");
+let categoriaSeleccionada = "1";
+categoria.addEventListener('change', ()=>{
+    categoriaSeleccionada = categoria.value;
+});
 
-//console.log(goToBuyTickets);
+let monto = document.getElementById("resumen");
+
+let nodoForm = document.getElementById("formulario");
+nodoForm.addEventListener('submit', ()=>{
+    event.preventDefault();
+
+    let totalEntrada;
+    switch ((parseInt(categoriaSeleccionada))) {
+        case 1:
+            totalEntrada = (cantTickets * 200) * 0.2;
+            break;
+        case 2:
+            totalEntrada = (cantTickets * 200) * 0.5;
+            break;
+        case 3:
+            totalEntrada = (cantTickets * 200) * 0.85;
+            break;
+    }
+    monto.innerHTML = "Total a Pagar: " + totalEntrada + "$";
+    console.log(totalEntrada)
+})
